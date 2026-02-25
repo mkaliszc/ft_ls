2db7e7052abf3cbd7459bbecd701ffde90fc83fa
mkaliszc <milan.kaliszczak@gmail.com.com>
2026-02-25 16:11:00 +0100

    + | SUID SGID S_ISVTX and more perms added

--- src/utils/long_format_utils/print_perm.c
+++ src/utils/long_format_utils/print_perm.c
@@ -26,26 +26,40 @@ void	print_link(t_dir_content *cur)
 
 void	print_perm(struct stat file_info)
 {
-	int	mode[] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
-	char	perm_char[] = {'r', 'w', 'x'};
+	char	perms[10];
 
-	if (S_ISDIR(file_info.st_mode)) {
+	if (S_ISDIR(file_info.st_mode))
 		write(1, "d", 1);
-	}
-	else if (S_ISLNK(file_info.st_mode)) {
+	else if (S_ISLNK(file_info.st_mode))
 		write(1, "l", 1);
-	}
-	else {
+	else if (S_ISBLK(file_info.st_mode))
+		write(1, "b", 1);
+	else if (S_ISCHR(file_info.st_mode))
+		write(1, "c", 1);
+	else if (S_ISFIFO(file_info.st_mode))
+		write(1, "p", 1);
+	else if (S_ISSOCK(file_info.st_mode))
+		write(1, "s", 1);
+	else
 		write(1, "-", 1);
-	}
-	for (int i = 0; i < 9; i++)
-	{
-		if (file_info.st_mode & mode[i]) {
-			write(1, &perm_char[i % 3], 1);
-		}
-		else {
-			write(1, "-", 1);
-		}
-	}
-	write(1, " ", 1);
+
+	perms[0] = (file_info.st_mode & S_IRUSR) ? 'r' : '-';
+	perms[1] = (file_info.st_mode & S_IWUSR) ? 'w' : '-';
+	perms[2] = (file_info.st_mode & S_IXUSR) ? 'x' : '-';
+	perms[3] = (file_info.st_mode & S_IRGRP) ? 'r' : '-';
+	perms[4] = (file_info.st_mode & S_IWGRP) ? 'w' : '-';
+	perms[5] = (file_info.st_mode & S_IXGRP) ? 'x' : '-';
+	perms[6] = (file_info.st_mode & S_IROTH) ? 'r' : '-';
+	perms[7] = (file_info.st_mode & S_IWOTH) ? 'w' : '-';
+	perms[8] = (file_info.st_mode & S_IXOTH) ? 'x' : '-';
+	perms[9] = '\0';
+
+	if (file_info.st_mode & S_ISUID)
+		perms[2] = (file_info.st_mode & S_IXUSR) ? 's' : 'S';
+	if (file_info.st_mode & S_ISGID)
+		perms[5] = (file_info.st_mode & S_IXGRP) ? 's' : 'S';
+	if (file_info.st_mode & S_ISVTX)
+		perms[8] = (file_info.st_mode & S_IXOTH) ? 't' : 'T';
+
+	ft_printf("%s ", perms);
 }
